62331dd4e7c004967476c88ab51cfe928a9771d1
dirkarnez <smalldirkalex@gmail.com>
2023-08-08 21:23:11 +0800

    - init template

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("hello world")
+from pdfminer.high_level import extract_pages
+from pdfminer.layout import LTLine
+
+for page_layout in extract_pages("10064689564_0.pdf"):
+    for element in page_layout:
+        # if isinstance(element, LTLine):
+            print(element)
--- run.cmd
+++ run.cmd
@@ -2,6 +2,6 @@
 set PYTHON_DIR=%USERPROFILE%\Downloads\python-3.10.8-amd64-portable
 set PATH=%PYTHON_DIR%;%PYTHON_DIR%\Scripts
 
-python main.py
-
+@REM python main.py
+python %PYTHON_DIR%\Scripts\pdf2txt.py 10064689564_0.pdf --output-dir cats-and-dogs
 pause
